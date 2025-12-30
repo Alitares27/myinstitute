202671fd1c948ea78d7453139d424dccd6b41a97
Alitares27 <aldair2795@hotmail.com>
2025-12-29 23:25:40 -0300

    refactor: Update CORS origin to correct URL for improved security

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://myinstitute-three.vercel.app"
+    "https://intitutoas-three.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
